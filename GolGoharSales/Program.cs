781dfafcee8c91e2c61d253fa6eb451c891f2208
Amir2170 <amirrezaamp2170@gmail.com>
2023-07-31 14:39:00 -0700

    successfully added 7 in 1 scss

--- GolGoharSales/Program.cs
+++ GolGoharSales/Program.cs
@@ -23,11 +23,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
